48582a2ca97c19907b92f22c60df3cf2900bb54a
Danicon <dancon.alferes@gmail.com>
2020-12-17 13:15:39 -0200

    continuação da Aula 16

---
+++ Aula16/ex08.py
@@ -0,0 +1,21 @@
+listabr = ('São Paulo', 'Atlético - MG', 'Flamengo', 'Palmeiras', 'Internacional', 'Grêmio', 'Fluminense', 'Santos', 'Atlético - GO', 'Corinthians', 'Ceará', 'Red Bull Bragantino', 'Fortaleza', 'Athletico Paranaense', 'Sport', 'Bahia', 'Vasco da Gama', 'Coritiba', 'Goiás', 'Botafogo')
+
+for c in range(0, len(listabr)):
+    if listabr[c] == 'Corinthians':
+        x = c
+
+print(30*'-=')
+print(f'Lista de times do Brasileirão: {listabr}')
+print(30*'-=')
+print(30*'-=')
+print(f'Os 5 primeiros são {listabr[:5]}')
+print(30*'-=')
+print(30*'-=')
+print(f'Os 4 últimos são {listabr[-4:]}')
+print(30*'-=')
+print(30*'-=')
+print(f'Times em ordem alfabética: {sorted(listabr)}')
+print(30*'-=')
+print(30*'-=')
+print(f'O Corinthians está na {x+1}ª posição')
+print(30*'-=')
---
+++ Aula16/ex12.py
@@ -0,0 +1,2 @@
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis', 'estudar', 'praticar', 'trabalhar', 'mercado', 'programador', 'futuro')
+
