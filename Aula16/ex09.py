913edb086915d0b4059a8b71f8ff8a6e257ee1bc
Danicon <dancon.alferes@gmail.com>
2020-12-18 12:42:18 -0200

    Finalização da Aula 16

--- Aula16/ex09.py
+++ Aula16/ex09.py
@@ -1,1 +1,22 @@
 from random import randint
+maior = menor = cont = 0
+numeros = (randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10), randint(1, 10))
+print(f'Os valores sorteados foram: ', end='')
+
+for n in numeros:
+    print(f'{n} ', end='')
+    if cont == 0:
+        maior = n
+        menor = n
+    else:
+        if n > maior:
+            maior = n
+        if n < menor:
+            menor = n
+    cont += 1
+print(f'\nO maior valor sorteado foi {maior}')
+print(f'O menor valor sorteado foi {menor}')
+
+
+# Ou print(f'\nO maior valor sorteado foi {max(numeros)}')
+# print(f'O menor valor sorteado foi {min(numeros)}')
