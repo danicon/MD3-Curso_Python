63eeb93e5df23e4f07f6c75dc024d5ff7cd9dcdd
Danicon <dancon.alferes@gmail.com>
2020-12-23 14:37:43 -0200

    Continuação da Aula17

--- Aula17/ex04.py
+++ Aula17/ex04.py
@@ -1,13 +1,24 @@
-nums = []
+nums = cont = []
 maior = menor = 0
 for num in range(0, 5):
     nums.append(int(input(f'Digite um valor para a Posição {num}: ')))
+    
+for cont, n in enumerate(nums):
+    if cont == 0:
+        maior = n
+        menor = n
+        ma = cont
+        me = cont
+    else:
+        if n > maior:
+            maior = n
+            ma = cont
+        if n < menor:
+            menor = n
+            me = cont
 
-for c, n in nums:
-    if max(nums):
-        x = c
 
 print(35*'=-')
 print(f'Você digitou os valores {nums}')
-print(f'Maior {x}')
-print(f'Menor {menor}')
+print(f'O maior valor digitado foi {maior} nas posição {ma}...')
+print(f'O menor valor digitado foi {menor} nas posição {me}...')
