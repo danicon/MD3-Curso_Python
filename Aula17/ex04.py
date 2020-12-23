ab7847714de3b9074ef22a8ca21bbd6f325006d1
Danicon <dancon.alferes@gmail.com>
2020-12-23 14:13:23 -0200

    commit

---
+++ Aula17/ex04.py
@@ -0,0 +1,13 @@
+nums = []
+maior = menor = 0
+for num in range(0, 5):
+    nums.append(int(input(f'Digite um valor para a Posição {num}: ')))
+
+for c, n in nums:
+    if max(nums):
+        x = c
+
+print(35*'=-')
+print(f'Você digitou os valores {nums}')
+print(f'Maior {x}')
+print(f'Menor {menor}')
