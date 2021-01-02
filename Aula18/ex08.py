8d9e837c08aaf49d313886bb002b3518da883c1e
Danicon <dancon.alferes@gmail.com>
2021-01-02 14:45:26 -0200

    Finalização da Aula 18

--- Aula18/ex08.py
+++ Aula18/ex08.py
@@ -1,16 +1,27 @@
 from random import randint
-mega = []
-sorteio = []
-jogos = int(input('Quantos jogos você quer que eu sortei? '))
-cont = 1 
-while cont < jogos:
-    for c in range(0, 6):
-        sorte = randint(1, 60)
-        sorteio.append(sorte)
-        mega.append(sorteio[:])
-        sorteio.clear()
-    cont += 1
-    
-
-print(sorteio)
-print(mega)
+from time import sleep
+lista = list()
+jogos = list()
+print('-'*30)
+print(f'{"JOGA NA MEGA SENA":^30}')
+print('-'*30)
+quant = int(input('Quantos jogos você quer que eu sortei? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-='*3, f'SORTEANDO {quant} JOGOS', '-='*3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i+1}: {l}')
+    sleep(1)
+print('-='*5, '< BOA SORTE! >', '-='*5)
