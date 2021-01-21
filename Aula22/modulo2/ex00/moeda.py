8e5da57fd8e1604e64797de33cc13cf5f3d29031
Danicon <dancon.alferes@gmail.com>
2021-01-21 12:54:20 -0200

    Inicio da Aula 22

---
+++ Aula22/modulo2/ex00/moeda.py
@@ -0,0 +1,16 @@
+def metade(m):
+    return m / 2
+
+
+def dobro(d):
+    return d * 2
+
+
+def aumentar(aument, valor):
+    tot = ((aument * valor) / 100) + aument
+    return tot
+
+
+def diminuir(diminui, valor):
+    tot = diminui - ((diminui * valor) / 100) 
+    return tot
